4b67b413bf5e5f54fdeaf8dffde5f9eaf53260ac
くろめ <ooi2572001@stu.o-hara.ac.jp>
2026-04-28 11:32:12 +0900

    SubjectListAction更新

--- src/main/java/scoremanager/main/SubjectListAction.java
+++ src/main/java/scoremanager/main/SubjectListAction.java
@@ -1,33 +1,53 @@
 package scoremanager.main;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.security.auth.Subject;
+
+import bean.Teacher;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import tool.Action;
 
 public class SubjectListAction extends Action {
 
+	//04/28 11:30更新　動くかは知らん
+	
 	@Override
 	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
 
-		//ローカル変数の宣言 1
-		//なし
+		HttpSession session = req.getSession();
+		Teacher teacher = (Teacher)session.getAttribute("user");
 
-		//リクエストパラメータ―の取得 2
-		//なし
+		// ローカル変数の指定 1
+		String subjectCd=null;
+		List<Subject> subjects = null; // 科目リスト
+		SubjectDao subjectDao = new SubjectDao(); // 科目Dao
+		String school;
+		Map<String, String> errors = new HashMap<>(); // エラーメッセージ
 
-		//DBからデータ取得 3
-		//なし
+		// リクエストパラメーターの取得 2
 
-		//ビジネスロジック 4
-		//なし
+		// ビジネスロジック 4
 
-		//DBへデータ保存 5
-		//なし
+		// DBからデータ取得 3
+		// ログインユーザーの学校コードを取得
+		school = teacher.getSchool().toString();
 
-		//レスポンス値をセット 6
-		//なし
+		subjects = subjectDao.filter(teacher.getSchool());
 
-		//JSPへフォワード 7
+		// レスポンス値をセット 6
+
+		// リクエストに科目リストをセット
+		req.setAttribute("subjects", subjects);
+		// リクエストにデータをセット
+		req.setAttribute("school", school);
+
+		// JSPへフォワード 7
 		req.getRequestDispatcher("subject_list.jsp").forward(req, res);
 	}
+
 }
